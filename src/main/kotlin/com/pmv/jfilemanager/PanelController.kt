91f6bc877d4efb9c515f5c94e32c02c3100ecf03
Matvei Popchenko <matvei.popchenko@gmail.com>
2022-10-12 15:12:02 +0300

    adding an enter to a folder by dbl click

--- src/main/kotlin/com/pmv/jfilemanager/PanelController.kt
+++ src/main/kotlin/com/pmv/jfilemanager/PanelController.kt
@@ -2,15 +2,16 @@ package com.pmv.jfilemanager
 
 import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.ComboBox
 import javafx.scene.control.TableCell
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import javafx.scene.control.TextField
 import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
+import java.nio.file.*
 import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.stream.Collectors
@@ -20,7 +21,14 @@ class PanelController: Initializable {
     @FXML
     lateinit var filesTable: TableView<FileInfo>
 
+    @FXML
+    lateinit var disksBox: ComboBox<String>
+
+    @FXML
+    lateinit var pathField: TextField
+
     override fun initialize(p0: URL?, p1: ResourceBundle?) {
+        //create columns in Table
         val fileTypeColumn = TableColumn<FileInfo, String>()
         fileTypeColumn.setCellValueFactory { param -> SimpleStringProperty(param.value.type.name) }
         fileTypeColumn.prefWidth = 24.0
@@ -48,6 +56,7 @@ class PanelController: Initializable {
         }
 
         val df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
         val fileDateColumn = TableColumn<FileInfo, String>("Дата изменений")
         fileDateColumn.setCellValueFactory { param -> SimpleStringProperty(param.value.lastModified.format(df)) }
         fileSizeColumn.prefWidth = 120.0
@@ -55,12 +64,53 @@ class PanelController: Initializable {
         filesTable.columns.addAll(fileTypeColumn, fileNameColumn, fileSizeColumn, fileDateColumn)
         filesTable.sortOrder.add(fileTypeColumn)
 
+        // add disk to ComboBox
+        disksBox.items.clear()
+        FileSystems.getDefault().rootDirectories.forEach {
+            disksBox.items.add(it.toString())
+        }
+        disksBox.selectionModel.select(0)
+
+        // двойной клик и переход если это каталог
+        filesTable.setOnMouseClicked() { mouseEvent ->
+            if (mouseEvent.clickCount == 2) {
+               val path = filesTable.selectionModel.let { Paths.get(pathField.text).resolve(it.selectedItem.filename) }
+
+               if (Files.isDirectory(path)) {
+                   updateList(path)
+               }
+           }
+        }
+
+        //update info in tables
         updateList(Paths.get("."))
     }
 
     private fun updateList(path: Path) {
+        pathField.text = path.normalize().toAbsolutePath().toString()
+
         filesTable.items.clear()
-        filesTable.items.addAll(Files.list(path).map { f -> FileInfo(f) }.collect(Collectors.toList()))
+
+        val fileList = mutableListOf<FileInfo>()
+        Files.list(path).forEach {
+            fileList.add(FileInfo(it))
+        }
+
+        filesTable.items.addAll(fileList)
         filesTable.sort()
+        filesTable.refresh()
+    }
+
+    fun onPathUpClick(actionEvent: ActionEvent) {
+        val upperPath = Paths.get(pathField.text).parent
+
+        if (upperPath != null) {
+            updateList(upperPath)
+        }
+    }
+
+    fun onSelectDisk(actionEvent: ActionEvent) {
+        val element = actionEvent.source as ComboBox<String>
+        updateList(Paths.get(element.selectionModel.selectedItem))
     }
 }
